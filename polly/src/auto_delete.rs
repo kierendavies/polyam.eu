3b7fe008d992979e14e0952e64f455ce231f23c5
Kieren Davies <kieren@kdavi.es>
2023-10-07 15:26:48 +0200

    Individually delete messages that are too old for bulk deletion

--- polly/src/auto_delete.rs
+++ polly/src/auto_delete.rs
@@ -1,4 +1,35 @@
+use std::{convert::identity, time::Duration};
+
+use poise::serenity_prelude::{ChannelId, Message};
+
 use crate::{context::Context, error::Result};
+
+async fn delete_messages(
+    ctx: &impl Context,
+    now: chrono::DateTime<chrono::Utc>,
+    channel_id: ChannelId,
+    messages: &[Message],
+) -> Result<()> {
+    // "You can only bulk delete messages that are under 14 days old."
+    // Pad the cutoff by one minute.
+    const BULK_DELETE_MAX_AGE: Duration = Duration::from_secs((14 * 24 * 60 * 60) - 60);
+
+    let bulk_cutoff = now - BULK_DELETE_MAX_AGE;
+    let bulk_cutoff_index = messages
+        .binary_search_by_key(&bulk_cutoff, |msg| *msg.timestamp)
+        .unwrap_or_else(identity);
+    let (individually, bulk) = messages.split_at(bulk_cutoff_index);
+
+    for msg in individually {
+        channel_id.delete_message(ctx.serenity(), msg.id).await?;
+    }
+
+    if !bulk.is_empty() {
+        channel_id.delete_messages(ctx.serenity(), bulk).await?;
+    }
+
+    Ok(())
+}
 
 pub async fn auto_delete(ctx: &impl Context) -> Result<()> {
     let now = chrono::Utc::now();
@@ -7,33 +38,28 @@ pub async fn auto_delete(ctx: &impl Context) -> Result<()> {
         let cutoff = now - cfg.after;
 
         loop {
-            // Repeatedly get the oldest messages.
-            let messages = cfg
+            // Repeatedly get the oldest messages that have not been deleted.
+            let mut batch = cfg
                 .channel
                 .messages(ctx.serenity(), |b| b.after(0).limit(100))
                 .await?;
 
-            let n_messages = messages.len();
+            batch.sort_by_key(|msg| msg.timestamp);
 
-            let delete_ids = messages
-                .into_iter()
-                .filter(|msg| *msg.timestamp < cutoff)
-                .map(|msg| msg.id)
-                .collect::<Vec<_>>();
+            let cutoff_index = batch
+                .binary_search_by_key(&cutoff, |msg| *msg.timestamp)
+                .unwrap_or_else(identity);
+            let to_delete = &batch[..cutoff_index];
 
-            let n_delete = delete_ids.len();
-
-            // Stop if there's nothing to delete.
-            if n_delete == 0 {
+            // Stop if we have run out of messages to delete.
+            if to_delete.is_empty() {
                 break;
             }
 
-            cfg.channel
-                .delete_messages(ctx.serenity(), &delete_ids)
-                .await?;
+            delete_messages(ctx, now, cfg.channel, to_delete).await?;
 
-            // If we already found a message that is too new, then all subsequent messages will also be too new.
-            if n_delete < n_messages {
+            // If we already received any message that is too new, then all subsequent messages will also be too new.
+            if to_delete.len() < batch.len() {
                 break;
             }
         }
